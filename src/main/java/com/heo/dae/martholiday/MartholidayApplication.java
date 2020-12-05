2526600217f4b8a9058687f9c1ce0287fda30e32
devdevdev09 <devdevdevdevdev09@gmail.com>
2020-12-05 22:33:39 +0900

    first commit

---
+++ src/main/java/com/heo/dae/martholiday/MartholidayApplication.java
@@ -0,0 +1,13 @@
+package com.heo.dae.martholiday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartholidayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartholidayApplication.class, args);
+	}
+
+}
